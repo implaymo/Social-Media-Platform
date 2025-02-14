b92114d35ed38c119674238a4ff2998399beb666
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-14 11:20:19 +0000

    Implement tests and algorithm of PasswordHash class

--- src/main/java/com/SocialMediaPlatform/PasswordEncryption/PasswordHash.java
+++ src/main/java/com/SocialMediaPlatform/PasswordEncryption/PasswordHash.java
@@ -1,6 +1,29 @@
 package com.SocialMediaPlatform.PasswordEncryption;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
 public class PasswordHash {
 
+    private static final Integer iterationCount = 65536;
+    private static final Integer keyLenght = 256;
 
+    public PasswordHash() {
+
+    }
+
+
+    public String generateHashPassword(String password, byte[] salt) {
+        try {
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, iterationCount, keyLenght);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/test/java/com/SocialMediaPlatform/PasswordEncryption/PasswordHashTest.java
+++ src/test/java/com/SocialMediaPlatform/PasswordEncryption/PasswordHashTest.java
@@ -1,7 +1,54 @@
 package com.SocialMediaPlatform.PasswordEncryption;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordHashTest {
 
+    private PasswordHash passwordHash;
+    private PasswordSalt passwordSalt;
+    private String password;
+
+    @BeforeEach
+    void setUp() {
+        passwordHash = new PasswordHash();
+        passwordSalt = new PasswordSalt();
+        password = "Password123!";
+    }
+
+
+    @Test
+    void shouldGenerateHashPassword() {
+        // arrange
+        byte[] salt = passwordSalt.generateRandomSalt();
+        // act
+        String hashedPassword1 = passwordHash.generateHashPassword(password, salt);
+        // assert
+        assertNotNull(hashedPassword1);
+    }
+
+    @Test
+    void shouldGenerateSamePasswordIfSaltIsEqual() {
+        // arrange
+        byte[] salt = passwordSalt.generateRandomSalt();
+        String hashedPassword1 = passwordHash.generateHashPassword(password, salt);
+        // act
+        String hashedPassword2 = passwordHash.generateHashPassword(password, salt);
+        // assert
+        assertEquals(hashedPassword1, hashedPassword2);
+    }
+
+    @Test
+    void shouldGenerateDifferentPasswordIfSaltIsDifferent() {
+        // arrange
+        byte[] salt1 = passwordSalt.generateRandomSalt();
+        byte[] salt2 = passwordSalt.generateRandomSalt();
+        String hashedPassword1 = passwordHash.generateHashPassword(password, salt1);
+        // act
+        String hashedPassword2 = passwordHash.generateHashPassword(password, salt2);
+        // assert
+        assertNotEquals(hashedPassword1, hashedPassword2);
+    }
 }
