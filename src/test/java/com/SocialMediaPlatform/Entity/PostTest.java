f8470b3d652320d7caa8e35a7c35709dc8f81a85
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-17 09:42:47 +0000

    Create Post tests and algorithm.

--- src/test/java/com/SocialMediaPlatform/Entity/PostTest.java
+++ src/test/java/com/SocialMediaPlatform/Entity/PostTest.java
@@ -10,11 +10,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class PostTest {
 
     private LocalDateTime specificDateTime;
+    private Post post;
 
     @BeforeEach
     void setUp() {
         specificDateTime = LocalDateTime.of(2025, 2, 17, 9, 36, 0);
-        Post post = Post.builder()
+        post = Post.builder()
                 .content("Hello World")
                 .mediaUrl("http://exampleimage.jpg")
                 .timestamp(specificDateTime)
@@ -25,7 +26,10 @@ class PostTest {
     @Test
     void shouldReturnValidPost(){
         // arrange
-        // act
-        // assert
+        // act + assert
+        assertNotNull(post);
+        assertEquals("Hello World", post.getContent());
+        assertEquals("http://exampleimage.jpg", post.getMediaUrl());
+        assertEquals(specificDateTime, post.getTimestamp());
     }
 }
