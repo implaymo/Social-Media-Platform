d0241282c3d52b0fc6acadc6abf8bc40fbe7386a
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-17 10:41:50 +0000

    Add necessary tests for my post entity. Add some requisite code to my PostRepository to make it a repository.

--- src/main/java/com/SocialMediaPlatform/Repository/PostRepository.java
+++ src/main/java/com/SocialMediaPlatform/Repository/PostRepository.java
@@ -1,5 +1,10 @@
 package com.SocialMediaPlatform.Repository;
 
-public interface PostRepository {
+import com.SocialMediaPlatform.Entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, String> {
 
 }
--- src/test/java/com/SocialMediaPlatform/Entity/PostTest.java
+++ src/test/java/com/SocialMediaPlatform/Entity/PostTest.java
@@ -1,35 +1,43 @@
-//package com.SocialMediaPlatform.Entity;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.time.Instant;
-//import java.time.LocalDateTime;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class PostTest {
-//
-//    private Instant createdAt;
-//    private Post post;
-//
-//    @BeforeEach
-//    void setUp() {
-//        post = Post.builder()
-//                .content("Hello World")
-//                .mediaUrl("http://exampleimage.jpg")
-//                .createdAt()
-//                .build();
-//    }
-//
-//
-//    @Test
-//    void shouldReturnValidPost(){
-//        // arrange
-//        // act + assert
-//        assertNotNull(post);
-//        assertEquals("Hello World", post.getContent());
-//        assertEquals("http://exampleimage.jpg", post.getMediaUrl());
-//        assertEquals(createdAt, post.getCreatedAt());
-//    }
-//}
+package com.SocialMediaPlatform.Entity;
+
+import com.SocialMediaPlatform.Repository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class PostTest {
+
+    @Test
+    void shouldCreateValidPost(){
+        // arrange
+        Post post = Post.builder()
+                .content("Hello World")
+                .mediaUrl("example.jpg")
+                .build();
+        // act + assert
+        assertNotNull(post);
+        assertEquals("Hello World", post.getContent());
+        assertEquals("example.jpg", post.getMediaUrl());
+    }
+
+    @Test
+    void shouldCreateValidPostIfMediaUrlNull(){
+        // arrange
+        Post post = Post.builder()
+                .content("Hello World")
+                .build();
+        // act + assert
+        assertNotNull(post);
+        assertEquals("Hello World", post.getContent());
+        assertNull(post.getMediaUrl());
+    }
+
+
+}
