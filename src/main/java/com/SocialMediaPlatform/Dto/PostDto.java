e070685a9f268f4d93f727233ba48c35c2e5460f
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-17 09:51:45 +0000

    Create tests and algorithm for PostDto.

--- src/main/java/com/SocialMediaPlatform/Dto/PostDto.java
+++ src/main/java/com/SocialMediaPlatform/Dto/PostDto.java
@@ -1,5 +1,16 @@
 package com.SocialMediaPlatform.Dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PostDto {
 
+    @NotBlank(message = "Content can't be null or blank")
+    private String content;
+
+    private String mediaUrl;
 }
--- src/test/java/com/SocialMediaPlatform/Dto/PostDtoTest.java
+++ src/test/java/com/SocialMediaPlatform/Dto/PostDtoTest.java
@@ -1,7 +1,44 @@
 package com.SocialMediaPlatform.Dto;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostDtoTest {
 
+    @Test
+    void shouldCreateValidPostDto(){
+        // arrange
+        PostDto postDto = PostDto.builder()
+                .content("Hello World")
+                .mediaUrl("http://exampleimage.jpg")
+                .build();
+        // act
+        // assert
+        assertNotNull(postDto);
+        assertEquals("Hello World", postDto.getContent());
+        assertEquals("http://exampleimage.jpg", postDto.getMediaUrl());
+    }
+
+    @Test
+    void shouldNotCreatePostDtoIfMissingContent(){
+        // arrange
+        PostDto postDto = null;
+        // act
+        // assert
+        assertNull(postDto);
+    }
+
+    @Test
+    void shouldCreatePostDtoIfHaveContentAndNotHaveMediaUrl(){
+        // arrange
+        PostDto postDto = PostDto.builder()
+                .content("Hello World")
+                .build();
+        // act
+        // assert
+        assertNotNull(postDto);
+        assertEquals("Hello World", postDto.getContent());
+        assertEquals(null, postDto.getMediaUrl());
+    }
 }
