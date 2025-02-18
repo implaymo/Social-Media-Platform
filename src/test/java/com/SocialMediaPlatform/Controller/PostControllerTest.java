4b9802effa8bc7480333ececb8e101976c435e29
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-18 10:19:26 +0000

    Create US_02 sequence diagram for method createPost

--- src/test/java/com/SocialMediaPlatform/Controller/PostControllerTest.java
+++ src/test/java/com/SocialMediaPlatform/Controller/PostControllerTest.java
@@ -1,7 +1,33 @@
 package com.SocialMediaPlatform.Controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnTrueIfPostCreated(){
+        // arrange
+        // act
+        // assert
+    }
+
+    @Test
+    void shouldReturnFalseIfDtoIsNull() {
+        // arrange
+        // act
+        // assert
+    }
+
+    @Test
+    void shouldReturnFalseIfInternalServiceError() {
+        // arrange
+        // act
+        // assert
+    }
 
 }
