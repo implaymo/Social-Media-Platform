f2de049bbc830c98eecaf289d51185a2dd97eb9b
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-21 11:27:24 +0000

    Create Like object and tests.

--- src/main/java/com/SocialMediaPlatform/Entity/Like.java
+++ src/main/java/com/SocialMediaPlatform/Entity/Like.java
@@ -1,4 +1,20 @@
 package com.SocialMediaPlatform.Entity;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+@Builder
 public class Like {
+
+    @Id
+    private String id;
+
+    @NotBlank(message =  "Post ID can't be null or blank")
+    private String postID;
+
+    @NotBlank(message =  "User ID can't be null or blank")
+    private String userID;
 }
--- src/test/java/com/SocialMediaPlatform/Entity/LikeTest.java
+++ src/test/java/com/SocialMediaPlatform/Entity/LikeTest.java
@@ -1,4 +1,55 @@
+package com.SocialMediaPlatform.Entity;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LikeTest {
-  
+
+
+    @Test
+    void shouldCreateValidLike(){
+        // arrange
+        Like like = Like.builder()
+                .postID("postID1")
+                .userID("userID1")
+                .build();
+        // act & assert
+        assertNotNull(like);
+    }
+
+    @Test
+    void shouldBuildLikeWithNullUserId(){
+        // arrange
+        Like like = Like.builder()
+                .postID("postID1")
+                .userID(null)
+                .build();
+        // act & assert
+        assertNull(like.getUserID());
+    }
+
+    @Test
+    void shouldBuildLikeWithNullPostId(){
+        // arrange
+        Like like = Like.builder()
+                .postID(null)
+                .userID("userID1")
+                .build();
+        // act & assert
+        assertNull(like.getPostID());
+    }
+
+    @Test
+    void shouldBuildLikeIfPostIdAndUserIdNull(){
+        // arrange
+        Like like = Like.builder()
+                .postID(null)
+                .userID(null)
+                .build();
+        // act & assert
+        assertNull(like.getPostID());
+        assertNull(like.getUserID());
+    }
+
 }
