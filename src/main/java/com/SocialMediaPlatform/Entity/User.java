a5b9f14e29bc99474d6f47dc2ef31fff2ab36079
Gonçalo Silva <1241908@isep.ipp.pt>
2025-02-22 19:24:09 +0000

    Implemented UserDetails

--- src/main/java/com/SocialMediaPlatform/Entity/User.java
+++ src/main/java/com/SocialMediaPlatform/Entity/User.java
@@ -3,11 +3,18 @@ package com.SocialMediaPlatform.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Document(collection = "user")
 @Data
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     private String id;
@@ -16,5 +23,40 @@ public class User {
     private String email;
     private String password;
     private String salt;
+    private ArrayList<Post> posts;
+
+    @Builder.Default
+    private boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
 
 }
