8e62a8233e40e087fcb60f4b607262cb6836e63a
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-02 17:59:13 +0000

    Remove unused import

--- src/main/java/com/SocialMediaPlatform/Service/UserLoginService.java
+++ src/main/java/com/SocialMediaPlatform/Service/UserLoginService.java
@@ -3,7 +3,6 @@ package com.SocialMediaPlatform.Service;
 import com.SocialMediaPlatform.Dto.UserLoginDto;
 import com.SocialMediaPlatform.Entity.User;
 import com.SocialMediaPlatform.Mapper.UserLoginMapper;
-import com.SocialMediaPlatform.PasswordEncryption.PasswordHash;
 import com.SocialMediaPlatform.Repository.UserRepository;
 import com.SocialMediaPlatform.Security.JWTUtil;
 import org.springframework.stereotype.Service;
