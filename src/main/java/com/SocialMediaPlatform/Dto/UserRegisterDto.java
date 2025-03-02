2a3bcb1a9adabde383512eaab29fe7edd942ce41
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-02 15:21:17 +0000

    update: remove unnecessary imports

--- src/main/java/com/SocialMediaPlatform/Dto/UserRegisterDto.java
+++ src/main/java/com/SocialMediaPlatform/Dto/UserRegisterDto.java
@@ -1,13 +1,11 @@
 package com.SocialMediaPlatform.Dto;
 
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 
 @Data
--- src/main/java/com/SocialMediaPlatform/Entity/User.java
+++ src/main/java/com/SocialMediaPlatform/Entity/User.java
@@ -1,11 +1,11 @@
 package com.SocialMediaPlatform.Entity;
 
-import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.data.annotation.Id;
 
 import java.util.ArrayList;
 import java.util.Collection;
