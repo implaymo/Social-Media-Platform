bc3b133ce4ea8d743157367eb75f3881f814c545
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-05 19:32:11 +0000

    test: Implement tests for CommentDto ci: Implement CommentDto

--- src/main/java/com/SocialMediaPlatform/Dto/CommentDto.java
+++ src/main/java/com/SocialMediaPlatform/Dto/CommentDto.java
@@ -1,6 +1,20 @@
 package com.SocialMediaPlatform.Dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CommentDto {
 
+    @NotBlank(message = "Post ID can't be null or blank")
+    private String postID;
+
+    @NotBlank(message = "User ID can't be null or blank")
+    private String userID;
+
+    @NotBlank(message = "Comment can't be null or blank")
     private String comment;
 }
--- src/test/java/com/SocialMediaPlatform/Dto/CommentDtoTest.java
+++ src/test/java/com/SocialMediaPlatform/Dto/CommentDtoTest.java
@@ -1,63 +1,91 @@
-//package com.SocialMediaPlatform.Dto;
-//
-//import jakarta.validation.Validator;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class CommentDtoTest {
-//
-//    private Validator validator;
-//
-//    @Test
-//    void shouldCreateValidCommentDto(){
-//        // arrange
-//        CommentDto commentDto = CommentDto.builder()
-//                .comment("YOLO!")
-//                .build();
-//        // act
-//        // assert
-//    }
-//
-//    @Test
-//    void shouldNotCreateCommentDtoWithCommentNull(){
-//        // arrange
-//        // act
-//        // assert
-//    }
-//
-//    @Test
-//    void shouldNotCreateCommentDtoWithCommentBlank(){
-//        // arrange
-//        // act
-//        // assert
-//    }
-//
-//    @Test
-//    void shouldNotCreateCommentDtoWithUserIdNull(){
-//        // arrange
-//        // act
-//        // assert
-//    }
-//
-//    @Test
-//    void shouldNotCreateCommentDtoWithUserIdBlank(){
-//        // arrange
-//        // act
-//        // assert
-//    }
-//
-//    @Test
-//    void shouldNotCreateCommentDtoWithUserIdNullAndCommentNull(){
-//        // arrange
-//        // act
-//        // assert
-//    }
-//
-//    @Test
-//    void shouldNotCreateCommentDtoWithUserIdBlankAndCommentBlank(){
-//        // arrange
-//        // act
-//        // assert
-//    }
-//}
+package com.SocialMediaPlatform.Dto;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentDtoTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void shouldCreateValidCommentDto(){
+        // arrange
+        String comment = "This is a comment";
+        String postID = "12345";
+        String userID = "userID";
+        // act
+        CommentDto commentDto = new CommentDto(postID, userID, comment);
+        Set<ConstraintViolation<CommentDto>> violations = validator.validate(commentDto);
+        // assert
+        assertNotNull(commentDto);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void shouldNotCreateCommentDtoWithCommentNull(){
+        // arrange
+        String postID = "12345";
+        String userID = "userID";
+        // act
+        CommentDto commentDto = new CommentDto(postID, userID, null);
+        Set<ConstraintViolation<CommentDto>> violations = validator.validate(commentDto);
+        // assert
+        assertTrue(violations.stream().anyMatch(v ->
+                v.getMessage().equals("Comment can't be null or blank")));
+    }
+
+    @Test
+    void shouldNotCreateCommentDtoWithCommentBlank(){
+        // arrange
+        String postID = "12345";
+        String userID = "userID";
+        CommentDto commentDto = new CommentDto(postID, userID, "");
+        // act
+        Set<ConstraintViolation<CommentDto>> violations = validator.validate(commentDto);
+        // assert
+        assertTrue(violations.stream().anyMatch(v ->
+                v.getMessage().equals("Comment can't be null or blank")));
+    }
+
+    @Test
+    void shouldNotCreateCommentDtoWithUserIdNull(){
+        // arrange
+        String comment = "This is a comment";
+        String postID = "12345";
+        CommentDto commentDto = new CommentDto(postID, null, comment);
+        // act
+        Set<ConstraintViolation<CommentDto>> violations = validator.validate(commentDto);
+        // assert
+        assertTrue(violations.stream().anyMatch(v ->
+                v.getMessage().equals("User ID can't be null or blank")));
+    }
+
+    @Test
+    void shouldNotCreateCommentDtoWithUserIdBlank(){
+        // arrange
+        String comment = "This is a comment";
+        String postID = "12345";
+        CommentDto commentDto = new CommentDto(postID, "", comment);
+        // act
+        Set<ConstraintViolation<CommentDto>> violations = validator.validate(commentDto);
+        // assert
+        assertTrue(violations.stream().anyMatch(v ->
+                v.getMessage().equals("User ID can't be null or blank")));
+    }
+
+
+}
