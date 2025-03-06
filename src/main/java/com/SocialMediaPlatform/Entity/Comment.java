fd5a462e2d72e884e3ad1701a4a0969362c857fe
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-06 20:07:50 +0000

    Add id field to comment

--- src/main/java/com/SocialMediaPlatform/Entity/Comment.java
+++ src/main/java/com/SocialMediaPlatform/Entity/Comment.java
@@ -3,10 +3,17 @@ package com.SocialMediaPlatform.Entity;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
+@Document(collection = "comment")
 public class Comment {
+
+    @Id
+    private String commentID;
+
 
     private String message;
     private String postID;
