58e10f95ece7da5a671232fac1d48a010261e760
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-06 20:09:22 +0000

    Create tests and implement CommentRepository

--- src/main/java/com/SocialMediaPlatform/Repository/CommentRepository.java
+++ src/main/java/com/SocialMediaPlatform/Repository/CommentRepository.java
@@ -1,5 +1,11 @@
 package com.SocialMediaPlatform.Repository;
 
-public interface CommentRepository {
+import com.SocialMediaPlatform.Entity.Comment;
+import com.SocialMediaPlatform.Entity.Like;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends MongoRepository<Comment, String> {
 
 }
--- src/test/java/com/SocialMediaPlatform/Repository/CommentRepositoryTest.java
+++ src/test/java/com/SocialMediaPlatform/Repository/CommentRepositoryTest.java
@@ -1,7 +1,54 @@
 package com.SocialMediaPlatform.Repository;
 
+import com.SocialMediaPlatform.Entity.Comment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataMongoTest
 class CommentRepositoryTest {
 
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    void shouldSaveCommentInDatabase() {
+        // arrange
+        Comment comment = Comment.builder().message("Hello World").postID("postID").userID("userID").build();
+        // act
+        Comment result = commentRepository.save(comment);
+        // assert
+        assertNotNull(result);
+        assertEquals("Hello World", result.getMessage());
+        assertEquals("postID", result.getPostID());
+        assertEquals("userID", result.getUserID());
+    }
+
+    @Test
+    void shouldFindCommentByPostID() {
+        // arrange
+        Comment comment = Comment.builder().commentID("commentID")
+                .message("Hello World")
+                .postID("postID")
+                .userID("userID")
+                .build();
+        commentRepository.save(comment);
+        // act
+        Optional<Comment> commentInDatabase = commentRepository.findById("commentID");
+        // assert
+        assertTrue(commentInDatabase.isPresent());
+    }
+
+    @Test
+    void shouldNotFindCommentByPostID() {
+        // arrange
+        // act
+        Optional<Comment> commentInDatabase = commentRepository.findById("NOT VALID POST ID");
+        // assert
+        assertTrue(commentInDatabase.isEmpty());
+    }
 }
