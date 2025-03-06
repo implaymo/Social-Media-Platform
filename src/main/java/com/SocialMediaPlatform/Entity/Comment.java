ec25f1155a43e60957cbbeb90ef230d498d52d08
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-06 19:50:29 +0000

    ci: Implement Comment

--- src/main/java/com/SocialMediaPlatform/Entity/Comment.java
+++ src/main/java/com/SocialMediaPlatform/Entity/Comment.java
@@ -1,4 +1,14 @@
 package com.SocialMediaPlatform.Entity;
 
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Comment {
+
+    private String message;
+    private String postID;
+    private String userID;
 }
