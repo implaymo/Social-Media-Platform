b8a8138028c2caf0e5cb9628f45d425e8f76a8d0
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-06 19:50:55 +0000

    Create tests and implement CommentMapper

--- src/main/java/com/SocialMediaPlatform/Mapper/CommentMapper.java
+++ src/main/java/com/SocialMediaPlatform/Mapper/CommentMapper.java
@@ -1,5 +1,21 @@
 package com.SocialMediaPlatform.Mapper;
 
+import com.SocialMediaPlatform.Dto.CommentDto;
+import com.SocialMediaPlatform.Entity.Comment;
+
 public class CommentMapper {
+
+    public Comment toEntity(CommentDto commentDto) {
+        if (commentDto == null) {
+            return null;
+        }
+        Comment comment = Comment.builder()
+                .message(commentDto.getComment())
+                .userID(commentDto.getUserID())
+                .postID(commentDto.getPostID())
+                .build();
+
+        return comment;
+    }
 
 }
--- src/test/java/com/SocialMediaPlatform/Mapper/CommentMapperTest.java
+++ src/test/java/com/SocialMediaPlatform/Mapper/CommentMapperTest.java
@@ -1,7 +1,43 @@
 package com.SocialMediaPlatform.Mapper;
 
+import com.SocialMediaPlatform.Dto.CommentDto;
+import com.SocialMediaPlatform.Entity.Comment;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CommentMapperTest {
 
+    @Test
+    void shouldTransformCommentDtoIntoCommentEntity() {
+        // arrange
+        CommentDto commentDto = mock(CommentDto.class);
+        when(commentDto.getComment()).thenReturn("Test comment");
+        when(commentDto.getUserID()).thenReturn("userID");
+        when(commentDto.getPostID()).thenReturn("postID");
+
+        CommentMapper commentMapper = new CommentMapper();
+
+        // act
+        Comment result = commentMapper.toEntity(commentDto);
+
+        // assert
+        assertNotNull(result);
+        assertEquals("Test comment", result.getMessage());
+        assertEquals("userID", result.getUserID());
+        assertEquals("postID", result.getPostID());
+    }
+
+    @Test
+    void shouldNotTransformCommentDtoIntoCommentEntityIfCommentDtoIsNull(){
+        // arrange
+        CommentMapper commentMapper = new CommentMapper();
+        CommentDto commentDto = null;
+        // act
+        Comment result = commentMapper.toEntity(commentDto);
+        // assert
+        assertNull(result);
+    }
 }
