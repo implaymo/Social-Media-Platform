be36f3b512bcefb5556456f210675ec18a17e111
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-07 12:12:34 +0000

    Create CustomUserDetails to get userID

---
+++ src/main/java/com/SocialMediaPlatform/Security/CustomUserDetails/CustomUserDetails.java
@@ -0,0 +1,73 @@
+package com.SocialMediaPlatform.Security.CustomUserDetails;
+
+import com.SocialMediaPlatform.Entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Component
+public class CustomUserDetails implements UserDetails {
+
+    private final String id;
+    private final String username;
+    private final String password;
+    private final Collection<GrantedAuthority> authorities;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getName();
+        this.password = user.getPassword();
+        this.authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
