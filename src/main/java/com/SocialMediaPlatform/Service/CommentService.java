4d97993730d1fcf6de4e957b0a5df87282153b49
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-07 09:42:16 +0000

    test: create CommentServiceTest ci: implement CommentService

--- src/main/java/com/SocialMediaPlatform/Service/CommentService.java
+++ src/main/java/com/SocialMediaPlatform/Service/CommentService.java
@@ -1,5 +1,33 @@
 package com.SocialMediaPlatform.Service;
 
+import com.SocialMediaPlatform.Dto.CommentDto;
+import com.SocialMediaPlatform.Entity.Comment;
+import com.SocialMediaPlatform.Mapper.CommentMapper;
+import com.SocialMediaPlatform.Repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class CommentService {
 
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentMapper commentMapper, CommentRepository commentRepository) {
+        this.commentMapper = commentMapper;
+        this.commentRepository = commentRepository;
+    }
+
+    public Optional<Comment> registerComment(CommentDto commentDto) {
+        if (commentDto == null) {
+            return Optional.empty();
+        }
+        Comment comment = commentMapper.toEntity(commentDto);
+        if (comment != null) {
+            commentRepository.save(comment);
+            return Optional.of(comment);
+        }
+        return Optional.empty();
+    }
 }
--- src/test/java/com/SocialMediaPlatform/Service/CommentServiceTest.java
+++ src/test/java/com/SocialMediaPlatform/Service/CommentServiceTest.java
@@ -1,7 +1,45 @@
 package com.SocialMediaPlatform.Service;
 
+import com.SocialMediaPlatform.Dto.CommentDto;
+import com.SocialMediaPlatform.Entity.Comment;
+import com.SocialMediaPlatform.Mapper.CommentMapper;
+import com.SocialMediaPlatform.Repository.CommentRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CommentServiceTest {
 
+
+    @Test
+    void shouldRegisterLikeInDatabase() {
+        // arrange
+        CommentDto commentDto = mock(CommentDto.class);
+        CommentMapper commentMapper = mock(CommentMapper.class);
+        CommentRepository commentRepository = mock(CommentRepository.class);
+        Comment comment = mock(Comment.class);
+        CommentService commentService = new CommentService(commentMapper, commentRepository);
+        when(commentMapper.toEntity(commentDto)).thenReturn(comment);
+        // act
+        Optional <Comment> result = commentService.registerComment(commentDto);
+        // assert
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    void shouldNotRegisterLikeInDatabaseIfCommentDtoIsNull() {
+        // arrange
+        CommentDto commentDto = null;
+        CommentMapper commentMapper = mock(CommentMapper.class);
+        CommentRepository commentRepository = mock(CommentRepository.class);
+        CommentService commentService = new CommentService(commentMapper, commentRepository);
+        // act
+        Optional<Comment> result = commentService.registerComment(commentDto);
+        // assert
+        assertTrue(result.isEmpty());
+    }
 }
