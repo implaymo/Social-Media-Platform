15330a1bd7b4bdb618b51baade235d12e82db66a
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-07 09:41:45 +0000

    Add bean to CommentMapper

--- src/main/java/com/SocialMediaPlatform/Mapper/CommentMapper.java
+++ src/main/java/com/SocialMediaPlatform/Mapper/CommentMapper.java
@@ -2,7 +2,9 @@ package com.SocialMediaPlatform.Mapper;
 
 import com.SocialMediaPlatform.Dto.CommentDto;
 import com.SocialMediaPlatform.Entity.Comment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentMapper {
 
     public Comment toEntity(CommentDto commentDto) {
