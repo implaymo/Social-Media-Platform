e23299f1e4c548dfe4a92f2f0ba63e65753c55db
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-08 11:30:02 +0000

    test: create tests for CommentController.
    ci: Implement CommentController

--- src/main/java/com/SocialMediaPlatform/Controller/CommentController.java
+++ src/main/java/com/SocialMediaPlatform/Controller/CommentController.java
@@ -1,5 +1,41 @@
 package com.SocialMediaPlatform.Controller;
 
+import com.SocialMediaPlatform.Dto.CommentDto;
+import com.SocialMediaPlatform.Entity.Comment;
+import com.SocialMediaPlatform.Security.CustomUserDetails.CustomUserDetails;
+import com.SocialMediaPlatform.Service.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class CommentController {
 
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/comment/{postID}")
+    public ResponseEntity<Boolean> registerComment(@Valid @RequestBody CommentDto commentDto, @PathVariable String postID) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() instanceof CustomUserDetails customUserDetails) {
+            String userID = customUserDetails.getId();
+            Optional<Comment> comment = commentService.registerComment(commentDto, postID, userID);
+            if (comment.isPresent()) {
+                return new ResponseEntity<>(true, HttpStatus.CREATED);
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(false);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+    }
 }
--- src/test/java/com/SocialMediaPlatform/Controller/CommentControllerTest.java
+++ src/test/java/com/SocialMediaPlatform/Controller/CommentControllerTest.java
@@ -1,7 +1,113 @@
 package com.SocialMediaPlatform.Controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.SocialMediaPlatform.Dto.CommentDto;
+import com.SocialMediaPlatform.Entity.Comment;
+import com.SocialMediaPlatform.Entity.User;
+import com.SocialMediaPlatform.Security.CustomUserDetails.CustomUserDetails;
+import com.SocialMediaPlatform.Service.CommentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class CommentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldRegisterComment() {
+        // arrange
+        User user = User.builder()
+                .id("userId")
+                .name("John")
+                .email("test@example.com")
+                .password("John@1234")
+                .build();
+        Comment comment = Comment.builder().commentID("commentId").message("message").build();
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+        UsernamePasswordAuthenticationToken auth =
+                new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String postID = "postId";
+        String userID = "userId";
+        CommentDto commentDto = mock(CommentDto.class);
+        CommentService commentService = mock(CommentService.class);
+        CommentController commentController = new CommentController(commentService);
+
+        when(commentService.registerComment(commentDto, postID, userID)).thenReturn(Optional.of(comment));
+
+        // act
+        ResponseEntity<Boolean> response = commentController.registerComment(commentDto, postID);
+
+        // assert
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertTrue(response.getBody());
+    }
+
+    @Test
+    void shouldNotRegisterCommentIfPostIdIsNull() {
+        // arrange
+        User mockUser = mock(User.class);
+
+        CustomUserDetails userDetails = new CustomUserDetails(mockUser);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String postID = "postId";
+        String userID = "userID";
+        CommentDto commentDto = mock(CommentDto.class);
+        CommentService commentService = mock(CommentService.class);
+        CommentController commentController = new CommentController(commentService);
+        when(commentService.registerComment(commentDto, postID, userID)).thenReturn(Optional.empty());
+        // act
+        ResponseEntity response = commentController.registerComment(commentDto, postID);
+        // assert
+        assertTrue(response.getStatusCode().is4xxClientError());
+    }
+
+    @Test
+    void shouldRejectInvalidComment() throws Exception {
+        String emptyJson = "{}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/comment/{postId}", "postId")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(emptyJson)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(SecurityMockMvcRequestPostProcessors.user(
+                                new CustomUserDetails(User.builder()
+                                        .id("userId")
+                                        .email("test@example.com")
+                                        .password("password")
+                                        .build())))
+                )
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+    }
 
 }
