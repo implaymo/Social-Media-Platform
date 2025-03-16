367cd2de882b9a2d2b5b012ea395ee10185e327e
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-16 11:02:58 +0000

    Create method to find like by post id and user id

--- src/main/java/com/SocialMediaPlatform/Repository/LikeRepository.java
+++ src/main/java/com/SocialMediaPlatform/Repository/LikeRepository.java
@@ -4,6 +4,9 @@ import com.SocialMediaPlatform.Entity.Like;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends MongoRepository<Like, String> {
+    Optional<Like> findByPostIdAndUserId(String postID, String userID);
 }
