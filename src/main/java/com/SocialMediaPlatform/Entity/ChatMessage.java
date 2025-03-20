339a17663b37ec40632e62bcdf88fd1932b9b317
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-20 11:15:23 +0000

    Create ChatMessage class and MessageType enum to determine the type of message the user receives

---
+++ src/main/java/com/SocialMediaPlatform/Entity/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.SocialMediaPlatform.Entity;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+}
