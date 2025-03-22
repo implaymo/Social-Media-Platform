8d1f720a2fe62c2d79362502e234c47bdaa69d62
Gonçalo Silva <1241908@isep.ipp.pt>
2025-03-22 17:53:42 +0000

    Add environmental loader to load all the variables needed for the application. Closes #1

--- src/main/java/com/SocialMediaPlatform/SocialMediaPlatformApplication.java
+++ src/main/java/com/SocialMediaPlatform/SocialMediaPlatformApplication.java
@@ -1,5 +1,6 @@
 package com.SocialMediaPlatform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -9,6 +10,9 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 public class SocialMediaPlatformApplication {
 
 	public static void main(String[] args) {
+		// Load environmental variables into application.
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(SocialMediaPlatformApplication.class, args);
 	}
 
