a1d9884e48e305484a191ed77579fdf028e1e831
Gonçalo Silva <1241908@isep.ipp.pt>
2025-04-04 17:51:32 +0100

    Create ChatController controller and unit tests #12

--- src/main/java/com/SocialMediaPlatform/Controller/ChatController.java
+++ src/main/java/com/SocialMediaPlatform/Controller/ChatController.java
@@ -1,18 +1,20 @@
 package com.SocialMediaPlatform.Controller;
 
 import com.SocialMediaPlatform.Entity.ChatMessage;
+import com.SocialMediaPlatform.Enum.MessageType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 
 @Controller
 public class ChatController {
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+    public ChatMessage sendMessage(@Validated @Payload ChatMessage chatMessage) {
 
         return chatMessage;
 
@@ -20,11 +22,16 @@ public class ChatController {
 
     @MessageMapping("/addUser")
     @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
-
+    public ChatMessage addUser(@Validated @Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        if(chatMessage == null || headerAccessor == null) {
+            throw new IllegalArgumentException("ChatMessage or headerAccessor is null ");
+        }
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        return chatMessage;
-
+        return ChatMessage.builder()
+                .type(MessageType.JOIN)
+                .sender("System")
+                .content(chatMessage.getSender() + " joined!")
+                .build();
     }
 }
