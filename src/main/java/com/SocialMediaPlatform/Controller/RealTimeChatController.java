dd199a56922dd73bc764e04caed46c5c899c2310
Gonçalo Silva <1241908@isep.ipp.pt>
2025-04-04 18:27:10 +0100

    Rename ChatController to RealTimeChatController

--- src/main/java/com/SocialMediaPlatform/Controller/RealTimeChatController.java
+++ src/main/java/com/SocialMediaPlatform/Controller/RealTimeChatController.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class RealTimeChatController {
 
-    @GetMapping("/")
-    public String home() {
-        return "index";  // This should resolve to /resources/templates/index.html
+    @GetMapping("/chat")
+    public String chat() {
+        return "chat";
     }
 }
